a1f69d121aa48b3a55edb60111da8ff9870ab2e4
sviatletsiuk-blip <sviatletsiuk@gmail.com>
2026-04-20 19:45:18 +0300

    commit

--- components/ErrorMessage/ErrorMessage.tsx
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import React from "react";
-import css from "./ErrorMessage.module.css"; // Переконайся, що назва файлу збігається
 
 interface ErrorMessageProps {
   children: React.ReactNode;
 }
 
 export default function ErrorMessage({ children }: ErrorMessageProps) {
-  return (
-    <div className={css.errorContainer} role="alert">
-      {children}
-    </div>
-  );
+  return <div role="alert">{children}</div>;
 }
